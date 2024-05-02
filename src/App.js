6ec293f2f82b450b76f3a5fd8b496706f8f86fae
Utkarsha Turambekar <utkarshat200318@gmail.com>
2024-05-02 22:56:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Slot = (props) => {
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
+
+  if ((x === y) && (y === z)) {
+    return (<>
+      <h1>{x} {y} {z}</h1>
+      <h3 className='head'> This is Matching</h3>
+    </>
+    )
+  }
+  else {
+    return (
+      <>
+        <h1>{x} {y} {z}</h1>
+        <h3 className='head'>This is not Matching</h3>
+      </>
+    )
+  }
 }
+
+const App = () => {
+  return (
+    <>
+      <h1 className='heading'>🎰 Welcome to <span style={{ fontWeight: "bold" }}>Slot Machine Game 🎰</span></h1>
+      <div className='body'>
+        <div className='slot'>  
+          <div className="item">
+          <hr />
+            <Slot x='😄' y='😄' z='😄' />
+            <hr />
+          </div>
+          <div className="item">
+            <Slot x='😄' y='😢' z='🎅' />
+            <hr />
+          </div>
+          <div className="item">
+            <Slot x='🍌' y='🍎' z='🥭' />
+            <hr />
+          </div>
+          <div className="item">
+            <Slot x='👼' y='👼' z='👼' />
+            <hr />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
